b3da7c16bd73e349dbf05ad49f59753cc4b063a9
phptuts <glaserpower@gmail.com>
2023-11-05 20:05:57 -0800

    10-user-validation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,18 @@
-const register = (request, response) => {
-  console.log(request.body, "user");
-  response.json(request.body);
+const { ValidationError } = require("yup");
+const { registerValidator } = require("../validators/user.validators");
+
+const register = async (request, response) => {
+  try {
+    await registerValidator.validate(request.body, { abortEarly: false });
+    console.log(request.body, "user");
+    response.json(request.body);
+  } catch (e) {
+    if (e instanceof ValidationError) {
+      response.status(400).send("bad request");
+      return;
+    }
+    throw e;
+  }
 };
 
 module.exports = { register };
