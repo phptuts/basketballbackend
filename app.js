38ecb317c01fe2948ad7d7d87b15104d921af8b5
phptuts <glaserpower@gmail.com>
2023-11-07 09:09:55 -0800

    16-register-form

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express");
 const app = express();
 const userRoutes = require("./routes/user.route");
 const { sequelize } = require("./database/init");
+var cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 app.use("/users", userRoutes);
 app.get("/", (req, res) => {
