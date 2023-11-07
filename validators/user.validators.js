b0e7dbd5ec6fbfc29aa672946a255800e86fb0d1
phptuts <glaserpower@gmail.com>
2023-11-07 08:27:09 -0800

    15-dupe-email

--- validators/user.validators.js
+++ validators/user.validators.js
@@ -1,7 +1,16 @@
 const yup = require("yup");
-
+const { UserModel } = require("../database/init");
 const registerValidator = yup.object({
-  email: yup.string().required().email().min(10),
+  email: yup
+    .string()
+    .required()
+    .email()
+    .min(5)
+    .test("unique-email", "Email is already taken.", async (value) => {
+      const user = await UserModel.findOne({ where: { email: value } });
+      // If nothing is returned the email is available.
+      return user === null || user === undefined;
+    }),
   password: yup.string().required().min(6).max(30),
 });
 
