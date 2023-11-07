505ffeee35d8cad0a9f5a1a106434e05b27ffe3d
phptuts <glaserpower@gmail.com>
2023-11-07 08:06:42 -0800

    14-bcrypt-hide-password

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,12 +3,18 @@ const { registerValidator } = require("../validators/user.validators");
 const formErrorsResponse = require("../responses/formerrors.response");
 const { UserModel } = require("../database/init");
 const createResponse = require("../responses/response");
+const bcrypt = require("bcrypt");
 
 const register = async (request, response) => {
   try {
-    await registerValidator.validate(request.body, { abortEarly: false });
-    const user = await UserModel.create(request.body);
-    response.status(201).json(createResponse("user", "create", user));
+    const userData = await registerValidator.validate(request.body, {
+      abortEarly: false,
+    });
+    userData.password = await bcrypt.hash(userData.password, 12);
+    const user = await UserModel.create(userData);
+    const userObj = user.toJSON();
+    delete userObj.password;
+    response.status(201).json(createResponse("user", "create", userObj));
   } catch (e) {
     if (e instanceof ValidationError) {
       response.status(400).json(formErrorsResponse(e));
