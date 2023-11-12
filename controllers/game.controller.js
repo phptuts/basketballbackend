c54a6fc0ab7c977ecbd9c2a21a575d28299c9e8d
phptuts <glaserpower@gmail.com>
2023-11-12 11:52:54 -0800

    29-create-game-table

--- controllers/game.controller.js
+++ controllers/game.controller.js
@@ -2,13 +2,16 @@ const { ValidationError } = require("yup");
 const formErrorsResponse = require("../responses/formerrors.response");
 const createResponse = require("../responses/response");
 const { addGameValidator } = require("../validators/game.validators");
+const { GameModel } = require("../database/init");
 
 const addGame = async (request, response) => {
   try {
     const validData = await addGameValidator.validate(request.body, {
       abortEarly: false,
     });
-    response.json(createResponse("game", "create", validData));
+    const newGameObj = { ...validData, isOver: false, isLive: false };
+    const game = await GameModel.create(newGameObj);
+    response.json(createResponse("game", "create", game.toJSON()));
   } catch (e) {
     if (e instanceof ValidationError) {
       response.status(400).json(formErrorsResponse(e));
